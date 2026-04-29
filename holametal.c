368f5da6e8fc99efd15f7ced49fb081902c2a843
benjamejia <benjamin16mejia16@gmail.com>
2026-04-29 00:19:48 -0600

    aprender tipos de datos stdint y manejo de overflow

--- holametal.c
+++ holametal.c
@@ -1,1 +1,28 @@
-int x = 5;
+#include <stdio.h>
+#include <stdint.h>
+
+void greet()
+{
+	printf("Tamños de datos <stdio.h>!\n");
+	printf("El tamaño de int: %d\n",sizeof(int));
+	printf("El tamaño de char: %d\n",sizeof(char));
+	printf("El tamaño de float: %d\n",sizeof(float));
+	printf("El tamaño de double: %d\n",sizeof(double));
+
+uint8_t pequeno = 255;
+uint32_t grande = 4000000000;	
+
+printf("Tamaños de datos fijos <stdint.h>!\n");
+	printf("El tamaño de uint8_t:%d\n",sizeof(pequeno));
+	printf("El tamaño de uint32_t:%d\n",sizeof(grande));
+
+pequeno = pequeno + 1; 
+    printf("Pequeño + 1: %u (¡Se desbordó a cero!)\n", pequeno);
+
+}
+
+int main()
+{
+	greet();
+	return 0;
+}
